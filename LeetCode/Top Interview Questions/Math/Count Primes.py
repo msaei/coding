4e57a48f1250b30a1602a8d34584fdc6fe9a2de9
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-12-24 17:54:08 -0500

    solution for Leetcode interview Math Count Primes

---
+++ LeetCode/Top Interview Questions/Math/Count Primes.py
@@ -0,0 +1,18 @@
+#Fizz Buzz
+#https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/744/
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        isPrime = [True] * n
+        counter = 0
+        for i in range(2, n):
+            if isPrime[i]:
+                counter += 1
+            else:
+                continue
+            for j in range(i, n, i):
+                isPrime[j] = False
+        return counter
+        
