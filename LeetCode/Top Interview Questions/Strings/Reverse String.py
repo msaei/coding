193f86153a463392093041c8d8057bbd8b1b0849
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-12-28 12:28:06 -0500

    solution for leetcode top interview question easy Strings Reverse String

---
+++ LeetCode/Top Interview Questions/Strings/Reverse String.py
@@ -0,0 +1,10 @@
+#Reverse String
+#https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/879/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        return
