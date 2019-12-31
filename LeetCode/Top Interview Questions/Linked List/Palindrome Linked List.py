52c00d0c80ac8b3cfa83f6fc79242c8ad7be490a
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-12-31 00:46:56 -0500

    solution for leetcode top interview question easy Linked List Palindrome Linked List

---
+++ LeetCode/Top Interview Questions/Linked List/Palindrome Linked List.py
@@ -0,0 +1,16 @@
+#Palindrome Linked List
+#https://leetcode.com/explore/featured/card/top-interview-questions-easy/93/linked-list/772/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals == vals[::-1]
