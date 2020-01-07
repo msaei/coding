4d258c894a95257f9862b342d2b53ba596dbee2b
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2020-01-07 17:04:31 -0500

    Solution for LeetCode Top Interview Arrays and Strings Group Anagrams

---
+++ LeetCode/Top Interview Questions/Arrays and Strings/Group Anagrams.py
@@ -0,0 +1,19 @@
+#Group Anagrams
+#https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/778/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key in anagrams.keys():
+                anagrams[key].append(word)
+            else:
+                anagrams[key] = [word]
+        return anagrams.values()
+
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
