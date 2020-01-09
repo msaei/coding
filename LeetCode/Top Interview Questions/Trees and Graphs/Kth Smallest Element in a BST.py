d9075bcd75de3da29135db8050d0df7e213cf584
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2020-01-09 16:08:16 -0500

    Solution for LeetCode Top Interview Trees and Graphs Kth Smallest Element in a BST

---
+++ LeetCode/Top Interview Questions/Trees and Graphs/Kth Smallest Element in a BST.py
@@ -0,0 +1,33 @@
+#Kth Smallest Element in a BST
+#https://leetcode.com/explore/interview/card/top-interview-questions-medium/108/trees-and-graphs/790/
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
