d06a280c4136696d766ae6433d1bdc0a9ebadad0
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2020-01-13 22:51:40 -0500

    solution for Leetcode Top Interview Questions Sorting and Searching Find Peak Element optimized

--- LeetCode/Top Interview Questions/Sorting and searching/Find Peak Element.py
+++ LeetCode/Top Interview Questions/Sorting and searching/Find Peak Element.py
@@ -2,6 +2,19 @@
 #https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/801/
 
 class Solution:
+    #binary search ln(n), 1
+    def findPeakElement(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        while(l<r):
+            mid = (l + r) // 2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+    #linear scan n, 1
     def findPeakElement(self, nums: List[int]) -> int:
         l = len(nums)
         for i in range(l-1):
