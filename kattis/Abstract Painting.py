64b5f35f893293a5708c9a96b32804178455eff8
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2020-01-19 23:27:49 -0500

    solution for Kattis Abstract Painting added

---
+++ kattis/Abstract Painting.py
@@ -0,0 +1,15 @@
+#Abstract Painting
+#https://open.kattis.com/problems/abstractpainting
+
+def getCombinations(rows: int, cols: int) -> int:
+    if rows * cols == 0:
+        return 0
+    a = (rows - 1) + (cols - 1)
+    b = (rows - 1) * (cols - 1)
+    return 18 * (6 ** a) * (2 ** b)
+
+n = int(input())
+for _ in range(n):
+    r,c = map(int, input().split())
+    print(getCombinations(r,c))
+
